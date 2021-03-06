bd2ea7c536c3260655b710d96dcd678ad8b4f1f6
JosiahAutor <autorjosiah@gmail.com>
2021-03-06 21:55:58 +0800

    generate body component and route to body

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { BodyComponent } from './components/body/body.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    BodyComponent
   ],
   imports: [
     BrowserModule,
